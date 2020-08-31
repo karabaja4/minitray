f686731ab981e4b6b24d36d1bea2e0ea34421aaa
Igor Saric <igor.saric@outlook.com>
2020-09-01 00:34:38 +0200

    a

--- main.cpp
+++ main.cpp
@@ -1,23 +1,31 @@
 #include <QApplication>
 #include <QIcon>
+#include <QDebug>
 #include <QProcess>
 #include <QSystemTrayIcon>
 
 void exec()
 {
-    QProcess *process = new QProcess();
     QStringList arguments = QCoreApplication::arguments();
-    QString program = arguments.at(1);
-    arguments.removeFirst();
-    arguments.removeFirst();
-    process->start(program, arguments);
+    if (arguments.count() > 1)
+    {
+        QProcess *process = new QProcess();
+        QString program = arguments.at(1);
+        arguments.removeFirst();
+        arguments.removeFirst();
+        process->start(program, arguments);
+    }
+    else
+    {
+        qInfo("no arguments passed");
+    }
 }
 
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
     QSystemTrayIcon *trayIcon = new QSystemTrayIcon();
-    trayIcon->setIcon(QIcon(":/images/heart.png"));
+    trayIcon->setIcon(QIcon(":/images/calendar.svg"));
     trayIcon->show();
     QObject::connect(trayIcon, &QSystemTrayIcon::activated, exec);
     return app.exec();
--- minitray.pro
+++ minitray.pro
@@ -2,8 +2,4 @@ SOURCES       = main.cpp
 RESOURCES     = minitray.qrc
 
 QT += widgets
-requires(qtConfig(combobox))
 
-# install
-target.path = $$[QT_INSTALL_EXAMPLES]/widgets/desktop/minitray
-INSTALLS += target
